f649719fe9f99853175584c28a0ef114becf4867
Taylor Gates <taylorang@msn.com>
2015-09-02 15:48:35 -0600

    Variables and Functions

---
+++ First Unity Project/New Unity Project/Assets/Third.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Third : MonoBehaviour {
+
+	int myInt = 5;
+	void Start () {
+		myInt = MultiplyByFour(myInt);
+		Debug.Log (myInt);
+	}
+	int MultiplyByFour(int number) {
+		int ret;
+		ret = number * 2;
+			return ret;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
