28ee1e71f1f6230d8d8d80d68ab2d5f434aea6c2
Taylor Gates <taylorang@msn.com>
2015-09-21 15:16:42 -0600

    fixing gitignore

---
+++ First Unity Project/New Unity Project/Assets/If Statement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatement : MonoBehaviour
+
+{
+	float HotChocolateTemperature = 80.0f;
+	float hotLimitTemperature = 85.0f;
+	float coldLimitTemperature = 50.0f;
+
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+		
+		HotChocolateTemperature -= Time.deltaTime * 5f;
+	}
+	void TemperatureTest ()
+	{
+		if(HotChocolateTemperature > hotLimitTemperature)
+		{
+			print("Hot Chocolate is too hot.");
+		}
+		else if(HotChocolateTemperature < coldLimitTemperature)
+		{
+			print("Hot Chocolate is too cold.");
+		}
+		else
+		{
+			print("Hot Chocolate is just right.");
+		}
+	}
+}
